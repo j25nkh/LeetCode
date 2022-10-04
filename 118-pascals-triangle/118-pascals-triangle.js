feeefc4f2e231f7001a63b7ab221f1d5d1225a98
j25nkh <105766632+j25nkh@users.noreply.github.com>
2022-10-04 18:10:18 +0900

    Time: 90 ms (53.34%), Space: 42.4 MB (7.37%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    var Result = [];
+    var ResultPre = [];
+    var ResultPart = [];
+
+    for (i = 0; i < numRows; i++) {
+        if (i == 0) {
+            Result.push([1]);
+            // ResultPre = Result;
+        } else if (i == 1) {
+            Result.push([1, 1]);
+        } else {
+            ResultPart = [];
+            ResultPre = Result[Result.length - 1];
+            for (j = 0; j < ResultPre.length - 1; j++) {
+                ResultPart.push(ResultPre[j] + ResultPre[j + 1])
+            }
+            ResultPart.push(1);
+            ResultPart.unshift(1);
+            Result.push(ResultPart);
+        }
+
+    }
+    return Result;
+};
