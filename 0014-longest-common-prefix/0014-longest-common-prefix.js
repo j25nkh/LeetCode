914fc7bdd75db84f51455c1931a6fef8cbd00d1d
j25nkh <105766632+j25nkh@users.noreply.github.com>
2022-10-29 21:42:05 +0900

    Time: 89 ms (74.93%), Space: 42.3 MB (70.97%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = "";
+    let Candidate = "";
+    
+//     if (strs.length === 1) {
+//         return prefix;
+//     }
+    
+    for (i = 0; i < strs[0].length; i++) {
+        Candidate = strs[0].charAt(i);
+        
+        for (j = 1; j < strs.length ; j++) {
+            if (strs[j].charAt(i) !== Candidate) {
+                return prefix;
+            }
+        }
+        
+        prefix = prefix + Candidate;
+    }
+        
+    return prefix;
+};
