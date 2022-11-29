e3f5069565f08ba05683c949820df5d7c98b22d9
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2022-11-29 21:09:57 +0900

    Time: 103 ms (66.33%), Space: 44.2 MB (54.28%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let sArr = s.split('').sort();
+    let tArr = t.split('').sort();
+    
+    for (i = 0; i < sArr.length; i++) {
+        if (sArr[i] !== tArr[i]) {
+            return tArr[i];
+        }
+    }
+    
+    return tArr[tArr.length - 1];
+};
