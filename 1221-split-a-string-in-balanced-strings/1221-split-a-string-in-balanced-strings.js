90c4adff4964150c0d709242f64527b63ed33f44
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-01-30 09:12:23 +0900

    Time: 76 ms (44.71%), Space: 41.9 MB (68.43%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -7,8 +7,8 @@ var balancedStringSplit = function(s) {
     let result = 0;
     
     for (let i = 0; i < s.length; i++) {
-        if (s.charAt(i) === "L") counter--;
-        if (s.charAt(i) === "R") counter++;
+        if (s[i] === "L") counter--;
+        if (s[i] === "R") counter++;
         
         if (counter === 0) result++;
         
