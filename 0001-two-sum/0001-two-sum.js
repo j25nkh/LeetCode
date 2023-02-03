524c4f5ac5a36f05d36916a425445437f3195fac
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-03 23:51:54 +0900

    Time: 65 ms (93.41%), Space: 43.1 MB (25.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,6 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.includes(target - nums[i]) && (i !== nums.indexOf(target - nums[i]))) {
+//             return [i, nums.indexOf(target - nums[i])];    
+//         }
+//     }
+    
+// };
+
 var twoSum = function(nums, target) {
   let cache = {};
 
