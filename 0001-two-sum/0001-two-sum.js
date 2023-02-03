257e8368b6f4476a7a023ab2d3ccb59624b5a1e0
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-04 00:01:05 +0900

    Time: 108 ms (58.13%), Space: 41.9 MB (95.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,25 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-// var twoSum = function(nums, target) {
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums.includes(target - nums[i]) && (i !== nums.indexOf(target - nums[i]))) {
-//             return [i, nums.indexOf(target - nums[i])];    
-//         }
-//     }
-    
-// };
+
 
 var twoSum = function(nums, target) {
-  let cache = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    let remaining = target - nums[i];
-      
-    if (cache[nums[i]] !== undefined) {
-      return [cache[nums[i]], i];
+    if (nums.length === 2) return [0, 1];
+    
+    // nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) return [i, j];
+            
+            // if (nums[j] > target - nums[i]) break;
+        }
     }
-
-    cache[remaining] = i;
-    }
+    
 };  
