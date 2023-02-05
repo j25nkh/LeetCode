951432f703901778e0de6b9b8d48e6d88b7be4d2
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-05 21:26:14 +0900

    Time: 85 ms (33.05%), Space: 41.9 MB (92.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,22 +3,42 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let str = s[0];
+//     let str = s[0];
     
+//     for (let i = 1; i < s.length; i++) {
+//         if (
+//             (str[str.length - 1] === '(' && s[i] === ')')
+//             || (str[str.length - 1] === '{' && s[i] === '}')
+//             || (str[str.length - 1] === '[' && s[i]  === ']')
+//         ) {
+//             str = str.slice(0, -1);
+//         } else {
+//             str = str + s[i];
+//         }
+//     }
+    
+//     if (str === "") return true;
+    
+//     return false;
+    
+    const stack = [s[0]];
+    
+        
     for (let i = 1; i < s.length; i++) {
         if (
-            (str[str.length - 1] === '(' && s[i] === ')')
-            || (str[str.length - 1] === '{' && s[i] === '}')
-            || (str[str.length - 1] === '[' && s[i]  === ']')
+            (stack[stack.length - 1] === '(' && s[i] === ')')
+            || (stack[stack.length - 1] === '{' && s[i] === '}')
+            || (stack[stack.length - 1] === '[' && s[i]  === ']')
         ) {
-            str = str.slice(0, -1);
+            stack.pop();
         } else {
-            str = str + s[i];
+            stack.push(s[i]);
         }
     }
     
-    if (str === "") return true;
+    if (stack.length === 0) return true;
     
     return false;
+    
     
 };
