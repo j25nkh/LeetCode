40400e04685f118a473ca48b0a5f72524459e90b
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-05 21:29:45 +0900

    Time: 61 ms (92.45%), Space: 42.2 MB (72.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,42 +3,42 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let str = s[0];
+//     let str = s[0];
     
-    for (let i = 1; i < s.length; i++) {
-        if (
-            (str[str.length - 1] === '(' && s[i] === ')')
-            || (str[str.length - 1] === '{' && s[i] === '}')
-            || (str[str.length - 1] === '[' && s[i]  === ']')
-        ) {
-            str = str.slice(0, -1);
-        } else {
-            str = str + s[i];
-        }
-    }
-    
-    if (str === "") return true;
-    
-    return false;
-    
-//     const stack = [s[0]];
-    
-        
 //     for (let i = 1; i < s.length; i++) {
 //         if (
-//             (stack[stack.length - 1] === '(' && s[i] === ')')
-//             || (stack[stack.length - 1] === '{' && s[i] === '}')
-//             || (stack[stack.length - 1] === '[' && s[i]  === ']')
+//             (str[str.length - 1] === '(' && s[i] === ')')
+//             || (str[str.length - 1] === '{' && s[i] === '}')
+//             || (str[str.length - 1] === '[' && s[i]  === ']')
 //         ) {
-//             stack.pop();
+//             str = str.slice(0, -1);
 //         } else {
-//             stack.push(s[i]);
+//             str = str + s[i];
 //         }
 //     }
     
-//     if (stack.length === 0) return true;
+//     if (str === "") return true;
     
 //     return false;
+    
+    const stack = [s[0]];
+    
+        
+    for (let i = 1; i < s.length; i++) {
+        if (
+            (stack[stack.length - 1] === '(' && s[i] === ')')
+            || (stack[stack.length - 1] === '{' && s[i] === '}')
+            || (stack[stack.length - 1] === '[' && s[i]  === ']')
+        ) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    
+    if (stack.length === 0) return true;
+    
+    return false;
     
     
 };
