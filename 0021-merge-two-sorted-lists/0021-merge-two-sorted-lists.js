1d8507b941b9e7171d70e315f98bc72875a594f1
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-12 12:18:48 +0900

    Time: 73 ms (67.38%), Space: 44.1 MB (57.20%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,15 +10,16 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-// var mergeTwoLists = function(list1, list2) {
-var mergeTwoLists = function (l1, l2) {
-    if (!l1) return l2;
-    else if (!l2) return l1;
-    else if (l1.val <= l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2
+var mergeTwoLists = function(list1, list2) {
+    if (!list1) return list2;
+    
+    if (!list2) return list1;
+    
+    if (list2.val > list1.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
     }
+    
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
 };
