9255c87873cf8c7271ec02a735b364f3455c1022
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-12 15:34:46 +0900

    Time: 92 ms (58.14%), Space: 51.9 MB (41.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,8 +5,7 @@
 var maxProfit = function(prices) {
     let min = prices[0];
     let maxProfit = 0;
-    
-    
+        
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] < min) {
             min = prices[i];
