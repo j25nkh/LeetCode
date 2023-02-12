85f7ea6557bbf77f0cbd4982e1e0ed7ecbf2d33a
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-12 15:35:17 +0900

    Time: 93 ms (54.20%), Space: 51.5 MB (74.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,17 +2,30 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let min = prices[0];
-    let maxProfit = 0;
+// var maxProfit = function(prices) {
+//     let min = prices[0];
+//     let maxProfit = 0;
         
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < min) {
-            min = prices[i];
-        } else if (prices[i] - min > maxProfit) {
-            maxProfit = prices[i] - min;
+//     for (let i = 1; i < prices.length; i++) {
+//         if (prices[i] < min) {
+//             min = prices[i];
+//         } else if (prices[i] - min > maxProfit) {
+//             maxProfit = prices[i] - min;
+//         }
+//     }
+    
+//     return maxProfit;
+// };
+
+var maxProfit = function(prices) {
+    let minPrice = Number.MAX_SAFE_INTEGER;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
         }
     }
-    
     return maxProfit;
 };
