78ceeba9120ea34cd7ad1a37261a2900936b62f1
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-18 21:44:50 +0900

    Time: 83 ms (51.19%), Space: 44 MB (87.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,7 +2,20 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {
-    let string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-    return string.split('').slice(0, Math.floor(string.length / 2)).every((letter, index) => letter === string[string.length - 1 - index])
+var isPalindrome = function(s) {
+    
+    // turn string to lowercase and use regex to remove non-alphanumeric
+    s = s.toLowerCase();
+    s = s.replace(/[^A-Za-z0-9]/g, '');
+
+    let start = 0;
+    let end = s.length-1; 
+    
+    while (start < end){
+        
+        if(s[start] !== s[end]) return false
+        start++;
+        end--;
+    }
+    return true
 };
