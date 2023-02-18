2a1a76638d75c6f9f8a0cf64868d202529cd0332
Jake Jeon <105766632+j25nkh@users.noreply.github.com>
2023-02-18 21:41:35 +0900

    Time: 89 ms (35.14%), Space: 46.5 MB (41.50%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,11 +5,15 @@
 var isPalindrome = function(s) {
     let forwardS = s.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
 
-    for (let i = 0; i < forwardS.length; i++) {
-        if (forwardS[i] !== forwardS[forwardS.length - 1 - i]) {
-            return false;
-        }
+//     for (let i = 0; i < forwardS.length; i++) {
+//         if (forwardS[i] !== forwardS[forwardS.length - 1 - i]) {
+//             return false;
+//         }
+//     }
+    
+    if (forwardS === forwardS.split("").reverse().join("")) {
+        return true;
     }
 
-    return true;
+    return false;
 };
